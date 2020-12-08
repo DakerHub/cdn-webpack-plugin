0e3d55a5f9e188e486b994ab1126f942ed02ffd8
daker <523246656@qq.com>
2020-12-08 18:44:46 +0800

    init project

---
+++ index.js
@@ -0,0 +1,9 @@
+const pluginName = 'CDNWebpackPlugin';
+
+class CDNWebpackPlugin {
+  apply(compiler) {
+    compiler.hooks.run.tap(pluginName, (compilation) => {
+      console.log('webpack 构建过程开始！');
+    });
+  }
+}
